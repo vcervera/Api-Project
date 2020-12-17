b4bc885f1ba3e9e0c7f74d08e3bcbe0b44439efd
Vanessa Cervera <vcervera01@saintmarys.edu>
2020-12-17 13:34:58 -0500

    modified

--- api.js
+++ api.js
@@ -1,20 +1,17 @@
 let baseURL = 'https://ghibliapi.herokuapp.com/films';
  
- 
-// let nameContainer = document.querySelector('.nameContainer');
-// let logo = document.getElementById('totLogo');
-// logo.addEventListener('click', fetchFilm)
- 
+
+let filminfo = document.querySelector('.filminfo');
 const searchTerm = document.querySelector('.search');
 const submitBtn = document.querySelector('.submit');
-const nextBtn = document.querySelector('.next');
+// const nextBtn = document.querySelector('.next');
 const searchForm = document.querySelector('form');
-const previousBtn = document.querySelector('.prev');
+// const previousBtn = document.querySelector('.prev');
  
-let pageNumber = 0;
+// let pageNumber = 0;
 searchForm.addEventListener('submit', fetchFilm);
-nextBtn.addEventListener('click', nextPage);
-previousBtn.addEventListener('click', previousPage);
+// nextBtn.addEventListener('click', nextPage);
+// previousBtn.addEventListener('click', previousPage);
  
 function fetchFilm(e) {
     e.preventDefault();
@@ -28,21 +25,26 @@ function fetchFilm(e) {
  
 function displayFilm(json) {
     console.log('Display Film', json);
+    let film= document.createElement('h1');
+    film.className= 'film';
+    film.innerText= json.title;
+    filminfo.appendChild(film);
+}
 }
 
 
-function nextPage(e) {
-    pageNumber++;
-    fetchFilm(e);
-    console.log('Page Number:', pageNumber);
-}
+// function nextPage(e) {
+//     pageNumber++;
+//     fetchFilm(e);
+//     console.log('Page Number:', pageNumber);
+// }
  
-function previousPage(e) {
-    if (pageNumber > 0) {
-        pageNumber--;
-    } else {
-        return;
-    }
-    fetchFilm(e);
-    console.log('Page:', pageNumber);
-} 
+// function previousPage(e) {
+//     if (pageNumber > 0) {
+//         pageNumber--;
+//     } else {
+//         return;
+//     }
+//     fetchFilm(e);
+//     console.log('Page:', pageNumber);
+// 
