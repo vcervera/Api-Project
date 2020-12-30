f3bf5103f7d9814ed244254135360c99a1cf27e0
Vanessa Cervera <vcervera01@saintmarys.edu>
2020-12-30 12:10:00 -0500

    Re-upload Api Project

--- public/api.js
+++ public/api.js
@@ -1,50 +1,97 @@
-let baseURL = 'https://ghibliapi.herokuapp.com/films';
- 
+let filmurl = 'https://ghibliapi.herokuapp.com/films';
+let locationurl='"https://ghibliapi.herokuapp.com/locations';
+// let filminfo = document.querySelector('.filminfo');
 
 let filminfo = document.querySelector('.filminfo');
-const searchTerm = document.querySelector('.search');
-const submitBtn = document.querySelector('.submit');
-// const nextBtn = document.querySelector('.next');
-const searchForm = document.querySelector('form');
-// const previousBtn = document.querySelector('.prev');
- 
-// let pageNumber = 0;
-searchForm.addEventListener('submit', fetchFilm);
-// nextBtn.addEventListener('click', nextPage);
-// previousBtn.addEventListener('click', previousPage);
- 
+let push = document.getElementById('button');
+push.addEventListener('click',fetchFilm);
+
+// let filmcar = document.querySelector('.filmcar');
+// let vehpic = document.getElementById('vehpic');
+// vehpic.addEventListener('click',fetchlocation);
+
+
 function fetchFilm(e) {
     e.preventDefault();
  
-    fetch(baseURL)
+    fetch(filmurl)
     .then(response => response.json())
     .then(json => displayFilm(json))
+
+    // let push = document.getElementById('button');
+    // push.removeChild(button);
  
 }
+
+function myFunction() {
+    var x = document.getElementById("button");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+// function fetchlocation(e) {
+//     e.preventDefault();
  
+//     fetch(locationurl)
+//     .then(response => response.json())
+//     .then(json => displaylocation (json))
+// } 
  
 function displayFilm(json) {
-    console.log('Display Film', json);
-    let film= document.createElement('h1');
-    film.className= 'film';
-    film.innerText= json.title;
-    filminfo.appendChild(film);
+    
+console.log('Display Film', json);
+ 
+
+for (i = 0; i < 20; i++ ){
+    console.log(json[i].title)
+ 
+    let pel = json[i].title 
+    let screen = document.createElement('h1')
+    screen.className = 'screen';
+    screen.innerText = `"${pel}"`;
+    
+ 
+    console.log(pel);
+    filminfo.appendChild(screen);}
+ 
 }
 
+// let button = document.createElement('button');
+// button.innerText = 'Click Me';
+// button.className = 'btn';
+// button.style = 'margin:0; margin-top: 500px;'
 
 
-// function nextPage(e) {
-//     pageNumber++;
-//     fetchFilm(e);
-//     console.log('Page Number:', pageNumber);
-// }
+// quoteContainer.appendChild(screen);
+// quoteContainer.appendChild(button)
+
+// button.addEventListener('click', () => {
+//     quoteContainer.removeChild(quote);
+//     quoteContainer.removeChild(button)
+
+//     displayfilm();
+// })
  
-// function previousPage(e) {
-//     if (pageNumber > 0) {
-//         pageNumber--;
-//     } else {
-//         return;
-//     }
-//     fetchFilm(e);
-//     console.log('Page:', pageNumber);
-// 
+// function displaylocation(json) {
+    
+// console.log('Display Location', json);
+ 
+
+// for (i = 0; i < 24; i++ ){
+//     console.log(json[i].location)
+ 
+//     let pel = json[i].location
+//     let screen = document.createElement('h1')
+//     screen.className = 'screen';
+//     screen.innerText = `"${pel}"`;
+    
+ 
+//     console.log(pel);
+//     filmcars.appendChild(screen);
+// }}
+
+
+
