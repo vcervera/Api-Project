ef0fafaf4433148ff56a04457a4686fd19c01251
Vanessa Cervera <vcervera01@saintmarys.edu>
2020-12-31 11:10:07 -0500

    Changes made

--- public/api.js
+++ public/api.js
@@ -6,9 +6,6 @@ let filminfo = document.querySelector('.filminfo');
 let push = document.getElementById('button');
 push.addEventListener('click',fetchFilm);
 
-// let filmcar = document.querySelector('.filmcar');
-// let vehpic = document.getElementById('vehpic');
-// vehpic.addEventListener('click',fetchlocation);
 
 
 function fetchFilm(e) {
@@ -18,13 +15,10 @@ function fetchFilm(e) {
     .then(response => response.json())
     .then(json => displayFilm(json))
 
-    // let push = document.getElementById('button');
-    // push.removeChild(button);
- 
 }
 
 function myFunction() {
-    var x = document.getElementById("button");
+    let x = document.getElementById("button");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
@@ -32,14 +26,6 @@ function myFunction() {
     }
   }
 
-// function fetchlocation(e) {
-//     e.preventDefault();
- 
-//     fetch(locationurl)
-//     .then(response => response.json())
-//     .then(json => displaylocation (json))
-// } 
- 
 function displayFilm(json) {
     
 console.log('Display Film', json);
@@ -58,40 +44,3 @@ for (i = 0; i < 20; i++ ){
     filminfo.appendChild(screen);}
  
 }
-
-// let button = document.createElement('button');
-// button.innerText = 'Click Me';
-// button.className = 'btn';
-// button.style = 'margin:0; margin-top: 500px;'
-
-
-// quoteContainer.appendChild(screen);
-// quoteContainer.appendChild(button)
-
-// button.addEventListener('click', () => {
-//     quoteContainer.removeChild(quote);
-//     quoteContainer.removeChild(button)
-
-//     displayfilm();
-// })
- 
-// function displaylocation(json) {
-    
-// console.log('Display Location', json);
- 
-
-// for (i = 0; i < 24; i++ ){
-//     console.log(json[i].location)
- 
-//     let pel = json[i].location
-//     let screen = document.createElement('h1')
-//     screen.className = 'screen';
-//     screen.innerText = `"${pel}"`;
-    
- 
-//     console.log(pel);
-//     filmcars.appendChild(screen);
-// }}
-
-
-
